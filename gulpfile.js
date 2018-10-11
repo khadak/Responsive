f5870cdb751c12d9f3ccc400919549b676924a80
khadak.k <khadak.k@flock.com>
2018-10-11 07:29:55 +0530

    Gulp task added

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,30 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var    autoprefixer = require('autoprefixer');
+var    mqpacker = require('css-mqpacker');
+
+
+    // folders
+ var   folder = {
+        src: 'src/',
+        build: 'build/'
+    };
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
+    var postCssOpts = [
+        autoprefixer({ browsers: ['last 2 versions', '> 2%'] }),
+        mqpacker
+    ];
     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
         .pipe(sass())
+        .pipe(postcss(postCssOpts))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
+
 
 
 // Move the javascript files into our /src/js folder
